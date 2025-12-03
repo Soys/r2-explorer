2f351a8d87584e5163277d5f717bb8b8e1af5144
soys <Soys@users.noreply.github.com>
2025-12-04 03:51:37 +0800

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,22 +1,10 @@
 import { R2Explorer } from "r2-explorer";
 
-const baseConfig = {
+// Read all available configurations here: https://r2explorer.com/getting-started/configuration/
+export default R2Explorer({
 	readonly: false,
-	cors: true,
-	showHiddenFiles: true,
-};
-
-export default {
-	async email(event, env, context) {
-		await R2Explorer(baseConfig).email(event, env, context);
-	},
-	async fetch(request, env, context) {
-		return R2Explorer({
-			...baseConfig,
-			basicAuth: {
-				username: env.BASIC_USERNAME,
-				password: env.BASIC_PASSWORD,
-			},
-		}).fetch(request, env, context);
-	},
-};
+	basicAuth: {
+	  username: 'molishark',
+	  password: 'Weare.520'
+	// }
+});
