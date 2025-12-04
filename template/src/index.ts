f10b58842ccb1442790304a87a47841031604689
soys <Soys@users.noreply.github.com>
2025-12-04 17:12:22 +0800

    Update index.ts

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,22 +1,3 @@
-import { R2Explorer } from "r2-explorer";
+import { R2Explorer } from 'r2-explorer';
 
-const baseConfig = {
-	readonly: false,
-	cors: true,
-	showHiddenFiles: true,
-};
-
-export default {
-	async email(event, env, context) {
-		await R2Explorer(baseConfig).email(event, env, context);
-	},
-	async fetch(request, env, context) {
-		return R2Explorer({
-			...baseConfig,
-			basicAuth: {
-				username: env.BASIC_USERNAME,
-				password: env.BASIC_PASSWORD,
-			},
-		}).fetch(request, env, context);
-	},
-};
+export default R2Explorer({ readonly: false, cfAccessTeamName: 'molishark' });
